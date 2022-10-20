3e721e8fab97688986dbcdd56b27c26ba40c7492
Minskoleopc <minskoleopc@gmail.com>
2022-10-20 22:33:53 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let ele = document.querySelector('#root')
+let root = ReactDOM.createRoot(ele)
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { latitude: null, errorMessage: ""}
+    // }
+    state = { latitude: null, errorMessage: ""}
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ latitude: position.coords.latitude })
+                console.log(position.coords.latitude)
+            }
+            ,
+            (err) => {
+                this.setState({ errorMessage: err.message})
+            }
+        )
+    }
+    render() {
+        // return (<div>
+        //     <div>Latitude:{this.state.latitude}</div>
+        //     <div>Error:{this.state.errorMessage}</div>
+        // </div>
+        // )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        if(this.state.errorMessage && !this.state.latitude){
+            return <div>Error:{this.state.errorMessage}</div>
+        }
+        else if(!this.state.errorMessage && this.state.latitude){
+            return <div>Latitude:{this.state.latitude}</div>
+        }
+        else {
+            return <div>Loading</div>
+        }
+
+    }
+
+
+
+}
+
+root.render(<App />)
+
+
+
+// latitute and no error message
+// errorMeassge and no latitue 
+// no error and no latitude
